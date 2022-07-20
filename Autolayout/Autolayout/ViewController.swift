75bde7fbdfb58e484fbd42cfca115c62ec7e22b5
ICMMAC03-BEBA <icmmac03-beba@ICMMAC03-BEBAs-Mac-mini.local>
2022-07-20 10:46:56 -0400

    AutoLayout

---
+++ Autolayout/Autolayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Autolayout
+//
+//  Created by ICMMAC03-BEBA on 20/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
