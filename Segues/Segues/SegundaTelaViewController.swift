e742154229a6566aabcae0e4399138c920e1e867
ICMMAC03-BEBA <icmmac03-beba@ICMMAC03-BEBAs-Mac-mini.local>
2022-07-20 11:31:02 -0400

    Segues

---
+++ Segues/Segues/SegundaTelaViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SegundaTelaViewController.swift
+//  Segues
+//
+//  Created by ICMMAC03-BEBA on 20/07/22.
+//
+
+import UIKit
+
+class SegundaTelaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    @IBAction func retornarsegundatela (segue: UIStoryboardSegue){
+    }
+    
+}
---
+++ Segues/Segues/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Segues
+//
+//  Created by ICMMAC03-BEBA on 20/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func retornarTelaPrincipal(segue: UIStoryboardSegue){
+    }
+    
+    
+}
+
